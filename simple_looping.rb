c7a78bb44b273b110b6ef28b00bdb844aac8f16d
Alison Golden <alisong779@yahoo.com>
2019-03-27 03:20:16 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -38,8 +38,7 @@ def for_iterator(number_of_times)
 end
 end
   
-  # code your solution here using the "for" keyword
-  # remember that `for` requires a range of numbers. How can we turn our number into a range?
+  
 
 
 
